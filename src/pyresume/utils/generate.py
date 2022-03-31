e5437ab19006a2038303e760a7ef3ae6da9ba0c4
Mark Klose <markklose17@gmail.com>
2022-03-31 19:59:43 -0400

    Begin Automated Resume PDF creation

--- src/pyresume/utils/generate.py
+++ src/pyresume/utils/generate.py
@@ -1,1 +1,20 @@
 # <script src="https://kit.fontawesome.com/8a4fc1c9ce.js" crossorigin="anonymous"></script>
+import dominate
+from dominate.tags import *
+
+
+def generate_cv(filename: str):
+    doc = dominate.document(title="CV test")
+
+    with doc.head:
+        link(rel='stylesheet', href='css/default.css')
+        script(type='text/javascript', src="https://kit.fontawesome.com/8a4fc1c9ce.js", crossorigin="anonymous")
+
+    with doc:
+        h1("header 1")
+
+    print(doc)
+
+
+def generate_resume(filename: str):
+    pass
