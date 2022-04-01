35ad4f544896a2a75dcb95d548d2b53b77c08468
Mark Klose <markklose17@gmail.com>
2022-03-31 20:48:21 -0400

    Able to create PDF files!

--- src/pyresume/utils/generate.py
+++ src/pyresume/utils/generate.py
@@ -2,19 +2,40 @@
 import dominate
 from dominate.tags import *
 
+from weasyprint import HTML
+from pathlib import Path
 
-def generate_cv(filename: str):
+
+def generate_cv(filename: str, css: str) -> str:
     doc = dominate.document(title="CV test")
 
     with doc.head:
-        link(rel='stylesheet', href='css/default.css')
+        link(rel='stylesheet', href=css)
         script(type='text/javascript', src="https://kit.fontawesome.com/8a4fc1c9ce.js", crossorigin="anonymous")
 
     with doc:
         h1("header 1")
+        p("This is some text. Wow.")
 
-    print(doc)
+        with div(id="aside", cls="section level1"):
+            h2("Contact Info")
+            p("My contact info")
+
+        print(doc)
+    return str(doc)
 
 
-def generate_resume(filename: str):
+def generate_resume(filename: str) -> str:
     pass
+
+
+def generate(filename: str = "details.csv", output: str = "cv.pdf", css: str = 'src/pyresume/css/default.css',
+             isCV: bool = True):
+    # Go through slightly different generation processes for different document types.
+    if isCV:
+        doc = generate_cv(filename, css)
+    else:
+        doc = generate_resume(filename, css)
+
+    dochtml = HTML(string=doc, base_url="")
+    Path(output).write_bytes(dochtml.write_pdf())
